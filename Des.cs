1a6f76f3d593a99ca9925e33870a65facda19f7c
michael <mykelgozie@gmail.com>
2021-01-06 18:32:21 +0100

    initial commit

---
+++ Des.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TripleDes
+{
+    public static  class Des
+    {
+
+      
+        private const string mysecurityKey = "15EA4CA20131C2FDDF2C13102AC4AE51";
+
+        public static string Encrypt(string TextToEncrypt)
+        {
+            byte[] MyEncryptedArray = UTF8Encoding.UTF8
+               .GetBytes(TextToEncrypt);
+
+            MD5CryptoServiceProvider MyMD5CryptoService = new
+               MD5CryptoServiceProvider();
+
+            byte[] MysecurityKeyArray = MyMD5CryptoService.ComputeHash
+               (UTF8Encoding.UTF8.GetBytes(mysecurityKey));
+
+           // MyMD5CryptoService.Clear();
+
+            var MyTripleDESCryptoService = new
+               TripleDESCryptoServiceProvider();
+            MyTripleDESCryptoService.BlockSize = 64;
+            MyTripleDESCryptoService.Key = MysecurityKeyArray;
+            MyTripleDESCryptoService.Padding = PaddingMode.None;
+            MyTripleDESCryptoService.Mode = CipherMode.ECB;
+          
+
+
+            var MyCrytpoTransform = MyTripleDESCryptoService
+               .CreateEncryptor();
+
+            byte[] MyresultArray = MyCrytpoTransform
+               .TransformFinalBlock(MyEncryptedArray, 0,
+               MyEncryptedArray.Length);
+
+            // MyTripleDESCryptoService.Clear();
+
+
+            string word = "";
+            foreach (var item in MyresultArray)
+            {
+
+                word += item;
+
+            }
+
+
+            return word;
+
+        }
+
+
+        public static string Encrypt2(string toEncrypt)
+        {
+            byte[] keyArray;
+            byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(toEncrypt);
+
+          
+           
+          
+              
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+
+            MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+            keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(mysecurityKey));
+            //Always release the resources and flush data
+            // of the Cryptographic service provide. Best Practice
+
+            hashmd5.Clear();
+            //set the secret key for the tripleDES algorithm
+            tdes.Key = keyArray;
+            //mode of operation. there are other 4 modes.
+            //We choose ECB(Electronic code Book)
+            tdes.Mode = CipherMode.ECB;
+            //padding mode(if any extra byte added)
+
+            tdes.Padding = PaddingMode.None;
+
+            ICryptoTransform cTransform = tdes.CreateEncryptor();
+            //transform the specified region of bytes array to resultArray
+            byte[] resultArray =
+              cTransform.TransformFinalBlock(toEncryptArray, 0,
+              toEncryptArray.Length);
+            //Release resources held by TripleDes Encryptor
+            tdes.Clear();
+            //Return the encrypted data into unreadable string format
+
+            string word = "";
+            foreach (var item in resultArray)
+            {
+
+                word += item;
+
+            }
+
+
+            return word;
+        }
+
+        public static string ByteArrayToHexString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+
+
+        public static string ConvertToBase64String(string text)
+        {
+
+          var bytesText =  Encoding.Unicode.GetBytes(text.ToUpper());
+
+          return  Convert.ToBase64String(bytesText);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TripleDes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        
+            //Console.WriteLine(Des.Encrypt2("041350A98789ABCD"));
+            // Console.WriteLine(Des.Encrypt("041350A98789ABCD"));
+
+            Console.WriteLine(Des.ConvertToBase64String("A33115C7FA282610"));
+
+
+
+
+        }
+    }
+}
